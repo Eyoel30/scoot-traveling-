3c24d493b674a57cd599aa011ee14edbbe240d6b
eyeol <eyoelzewde23@gmail.com>
2026-01-14 23:19:58 +0300

    minor css changes

--- src/components/Homepage-components/features.jsx
+++ src/components/Homepage-components/features.jsx
@@ -6,9 +6,10 @@ import "./features.css"
 export default function Features() {
   return (
     <section className="wrapper small features">
+      <div className="gray-line desktop"></div>
       <h2 className="visually-hidden">Features</h2>
-      <div className="gray-line"></div>
       <div className="features-sm">
+        <div className="gray-line tablet"></div>
         <FeatureSmall values={feature_small} />
       </div>
       <div className="features-lg">
