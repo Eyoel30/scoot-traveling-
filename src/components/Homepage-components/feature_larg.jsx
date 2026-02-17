4dda4b69896c4824a475d363b328a7c301b55c68
eyeol <eyoelzewde23@gmail.com>
2026-02-17 22:24:06 +0300

    horizontal scrole fix, navBar fix, layout update

--- src/components/Homepage-components/feature_larg.jsx
+++ src/components/Homepage-components/feature_larg.jsx
@@ -2,21 +2,26 @@ import Button from "../common/buttonlink"
 
 const buttonValue = { path: "/About", label: "Learn More", classname: "button" }
 
-export default function FeatureLarg({ values }) {
+export default function _rFeatureLarg({ values }) {
 
   return (
     <>
       {values.map((value) => (
         <div className="wrapper fb-col feature-lg">
+
           <img className="feature-lg-img" src={value.image} width="445" height="445" alt={value.alt} />
+
           <div className="feature-lg-text">
             <h3 className="feature-lg-title">{value.header}</h3>
             <p className="feature-lg-descr">{value.content}</p>
             <Button button={buttonValue} />
           </div>
-          <img className={value.arroclass_r} src={value.arrow_right} width={value.width} height={value.height}/>
-          <img className={value.arroclass_l} src={value.arrow_left} width={value.width} height={value.height}/>
 
+          <img className={value.arroclass_r} src={value.arrow_right} width={value.width_r} height={value.height_r} />
+          <img className={value.arroclass_l} src={value.arrow_left} width={value.width_l} height={value.height_l} />
+          
+          <img src={value.circleimage} className={value.circleClass} width={value.width_c} height={value.height_c} />
+          
         </div>
       )
       )}
--- src/data/feature_data.js
+++ src/data/feature_data.js
@@ -6,6 +6,8 @@ import nearyou from "../../assets/images/near-you.jpg"
 import payment from "../../assets/images/payments.jpg"
 import leftdownarrow from "../../assets/patterns/left-downward-arrow.svg"
 import rightarrow from "../../assets/patterns/right-arrow.svg"
+import circle from "../../assets/patterns/circle.svg"
+
 export const feature_small = [
   {
     image: local, header: "Locate with app", content: "Use the app to find the nearest scooter to you. We are continuously placing scooters in the areas with most demand, so one should never be too far away."
@@ -21,13 +23,13 @@ export const feature_small = [
 
 export const feature_larg = [
   {
-    image: telemetry,alt:"woment holding a phone", header: "Easy to use riding telemetry", content: "The Scoot app is available with riding telemetry. This means it can show you your average peed, how long you've been using the scooter, your traveling distance, and many more things all in an easy to use app.",  arrow_right:rightarrow, arroclass_r:"feature-lg-arrow-r", width:452, height:151,arrow_left:leftdownarrow, arroclass_l:"feature-lg-arrow-l", width:741, height:151
+    image: telemetry,alt:"woment holding a phone", header: "Easy to use riding telemetry", content: "The Scoot app is available with riding telemetry. This means it can show you your average peed, how long you've been using the scooter, your traveling distance, and many more things all in an easy to use app.",  arrow_right:rightarrow, arroclass_r:"feature-lg-arrow-r", width_r:452, height_r:151,arrow_left:leftdownarrow, arroclass_l:"feature-lg-arrow-l", width_l:741, height_l:151,circleimage:circle,circleClass:"feature-lg-c",width_c:445, height_c:445
   },
   {
-    image: nearyou,alt:"", header: "Coming to a city near you", content: "Scoot is available in 4 major cities so far. We’re expanding rapidly, so be sure to let us know if you want to see us in your hometown. We’re aiming to let our scooters loose on 23 cities over the coming year.", arrow_right:rightarrow, arroclass_r:"feature-lg-arrow-r", width:452, height:151, arrow_left:leftdownarrow, arroclass_l:"feature-lg-arrow-l", width:741, height:151
+    image: nearyou,alt:"", header: "Coming to a city near you", content: "Scoot is available in 4 major cities so far. We’re expanding rapidly, so be sure to let us know if you want to see us in your hometown. We’re aiming to let our scooters loose on 23 cities over the coming year.", arrow_right:rightarrow, arroclass_r:"feature-lg-arrow-r", width_r:452, height_r:151, arrow_left:leftdownarrow, arroclass_l:"feature-lg-arrow-l", width_l:741, height_l:151
   },
   {
-    image: payment,alt:"", header: "Zero hassle payments", content: "Our payment is as easy as one two three. We accept most credit cards and debit cards. You can also link your PayPal account inside the app.Need to pay later? No worries! You can defer payment for up to a month.", arrow_right:rightarrow, arroclass_r:"feature-lg-arrow-r", width:452, height:151,arrow_left:leftdownarrow, arroclass_l:"feature-lg-arrow-l", width:741, height:151
+    image: payment,alt:"", header: "Zero hassle payments", content: "Our payment is as easy as one two three. We accept most credit cards and debit cards. You can also link your PayPal account inside the app.Need to pay later? No worries! You can defer payment for up to a month.", arrow_right:rightarrow, arroclass_r:"feature-lg-arrow-r", width_r:452, height_r:151,arrow_left:leftdownarrow, arroclass_l:"feature-lg-arrow-l", width_l:741, height_l:151
   }
 ]
 
